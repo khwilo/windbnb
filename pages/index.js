5d379973bb2352392127ab2319a19f9e92b0363f
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-02 22:43:38 +0300

    Add class names to HTML

--- pages/index.js
+++ pages/index.js
@@ -8,141 +8,190 @@ export default function Home() {
         <link rel='icon' href='/images/devchallenges.png' />
       </Head>
 
-      <header>
-        <img src='/images/logo.svg' />
+      <header className='header'>
+        <img src='/images/logo.svg' className='logo-image' />
 
-        <div>
+        <div className='header__form'>
           {/* TODO: Add input fields here */}
           <p>Select input</p>
         </div>
       </header>
 
-      <main>
-        <h1>Stays in Finland</h1>
+      <main className='main'>
+        <h1 className='main__title'>Stays in Finland</h1>
 
-        <div>
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+        <div className='stays'>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Nice apartment in center of Helsinki</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
-              src='https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80'
+              className='section__img'
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
 
-          <section>
-            <h2>Stylist apartment in center of the city</h2>
+          <section className='section'>
+            <h2 className='section__title'>
+              Stylist apartment in center of the city
+            </h2>
             <img
+              className='section__img'
               src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
               alt=''
             />
-            <div>
-              <div>
-                <p>Super host</p>
+            <div className='stay'>
+              <div className='stay__tag-container'>
+                <p className='stay__tag-title'>Super host</p>
               </div>
-              <p>Entire apartment . 2 beds</p>
-              <div>
-                <img src='/images/star_rate.svg' alt='star rating icon' />
-                <p>4.40</p>
+              <p className='stay__type'>Entire apartment . 2 beds</p>
+              <div className='rating'>
+                <img
+                  className='rating__img'
+                  src='/images/star_rate.svg'
+                  alt='star rating icon'
+                />
+                <p className='rating__number'>4.40</p>
               </div>
             </div>
           </section>
