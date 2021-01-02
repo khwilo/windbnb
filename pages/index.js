59063dfda2bab9cc4906978c81902769b9c04be1
Khwilo Kabaka <khwilowatai@gmail.com>
2021-01-02 22:33:07 +0300

    Add minimal page structure

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,145 @@ export default function Home() {
         <link rel='icon' href='/images/devchallenges.png' />
       </Head>
 
+      <header>
+        <img src='/images/logo.svg' />
+
+        <div>
+          {/* TODO: Add input fields here */}
+          <p>Select input</p>
+        </div>
+      </header>
+
       <main>
-        <h1>Windbnb</h1>
+        <h1>Stays in Finland</h1>
+
+        <div>
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Nice apartment in center of Helsinki</h2>
+            <img
+              src='https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+
+          <section>
+            <h2>Stylist apartment in center of the city</h2>
+            <img
+              src='https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=2255&q=80'
+              alt=''
+            />
+            <div>
+              <div>
+                <p>Super host</p>
+              </div>
+              <p>Entire apartment . 2 beds</p>
+              <div>
+                <img src='/images/star_rate.svg' alt='star rating icon' />
+                <p>4.40</p>
+              </div>
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   );
